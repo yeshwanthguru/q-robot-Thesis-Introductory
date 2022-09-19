3e4830b9df12d4cc548edf7d2b1dc15870ac0049
yeshwanth guru krishnakumar <72270080+yeshwanthguru@users.noreply.github.com>
2022-09-19 19:30:25 +0200

    Final update of introductory_script

--- src/scripts/introductory_demo.py
+++ src/scripts/introductory_demo.py
@@ -2,48 +2,38 @@ import qrobot
 import json
 import numpy as np
 from qrobot.models import AngularModel
-from contextlib import redirect_stdout
 
-n=1 #switch data with an AngularModel with n=1
-tau=15 #temporal window
+n=1  #switch data with an AngularModel with n=1
+tau=15#temporal window
 
-#initialized an angular model
-model = qrobot.models.AngularModel(n,tau)
-sequence = list()  
+sequence = list()
 
-#start a loop for every sample period
-for time in range(0, model.tau):  # loop throug the event sequence
- 
-#A virtual switch yet to be done in the final script
-
-# Balanced events between the switch_on and switch_off(between 0 and 1)
- for i in range(1, int(tau / 2)):
+# Balanced events (between 0 and 1)
+for i in range(0, int(tau / 2)):
     sequence.append(np.random.randint(0, 1000) / 1000)
 
 # Unbalanced events (balanced between .5 and 1)
 for i in range(int(tau / 2), tau):
     sequence.append(np.random.randint(500, 1000) / 1000)
 
- 
+model = qrobot.models.AngularModel(n , tau) #Angular model is initialized
 
-#Encode the input in the model
-model.encode(sequence[time], dim=0)
+print(f"Hello model!\n{model}")
 
+#Start a loop: every sample period
+for t in range(model.tau): 
+     while (True):
+        model = AngularModel(n, tau)#This input is then encoded in the AngularModel
+        model.clear()
+        for t in range(0, model.tau): 
+            model.encode(sequence[t], dim=0)
+            model.print_circuit()
+            model.clear()#If we are at the end of the temporal window:
+            shots = 1  #Measure the module (Single shot)
+            counts = model.measure(shots)
+            print("Aggregated binary outcomes of the circuit:")
+            print(json.dumps(counts, sort_keys=True, indent=4))
+            #Reinitializing the model to encode a new temporal window
 
-model.print_circuit()
-
-#Measure the module (Single shot)
-shots = 1
-counts = model.measure(shots)
-
-
-#output
-with open('introductory_demo_out.txt', 'w') as f:
-     with redirect_stdout(f):
-          print("Aggregated binary outcomes of the circuit:")
-          print(json.dumps(counts, sort_keys=True, indent=4))
-
-#Reinitializing the model to encode a new temporal window
-
-model.clear()
+            model.clear()
 
