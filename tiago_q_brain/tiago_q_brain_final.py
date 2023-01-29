a079c6a3f815a517b06eb8262967ce2e7416429c
yeshwanth guru krishnakumar <72270080+yeshwanthguru@users.noreply.github.com>
2023-01-29 02:49:43 +0100

    Create tiago_q_brain_final.py

---
+++ tiago_q_brain/tiago_q_brain_final.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3.8
+import rospy
+import json
+import time
+import random
+import numpy as np
+from std_srvs.srv import Empty
+from qrobot.qunits import SensorialUnit
+from qrobot.models import AngularModel
+from qrobot.bursts import  OneBurst
+from geometry_msgs.msg import PoseArray
+from moveit_msgs.msg import PickupActionGoal, PlaceActionGoal
+
+# Initialize ROS node
+rospy.init_node('Tiago_brain', anonymous=True)
+n = 1  # number of qubits
+tau = 1  # number of events for each temporal window
+# model, burst and sampling time
+model = AngularModel(n, tau)
+burst = OneBurst()
+Ts = 0.5  # sample every 500ms
+
+# Initialize the sensorial units
+sensorial_unit1 = SensorialUnit("SensorialUnit1", Ts)
+sensorial_unit2= SensorialUnit("SensorialUnit2", Ts)
+sensorial_unit3 = SensorialUnit("SensorialUnit3", Ts)
+
+def callback_function(msg):
+    uncertain_value = (np.random.randint(500, 1000) / 1000)
+    sensorial_unit1.scalar_reading  = uncertain_value
+    model.encode(sensorial_unit1.scalar_reading , dim=0)
+    model.print_circuit()
+def pickup_callback(msg):
+    uncertain_value = (np.random.randint(500, 1000) / 1000)
+    sensorial_unit2.scalar_reading = uncertain_value
+    model.encode(sensorial_unit2.scalar_reading , dim=0)
+    model.print_circuit()
+def place_callback(msg):
+    uncertain_value = (np.random.randint(500, 1000) / 1000)
+    sensorial_unit3.scalar_reading = uncertain_value
+    model.encode(sensorial_unit3.scalar_reading , dim=0)
+    model.print_circuit()
+
+    
+def tiago_brain():
+    shots = 1
+    counts = model.measure(shots)
+    topic_active = False
+    while not rospy.is_shutdown():
+        try:
+            if not topic_active:
+                # Subscribe to the rostopics
+                rospy.Subscriber("/grasp_poses", PoseArray, callback_function)
+                rospy.Subscriber('/pickup/goal', PickupActionGoal, pickup_callback)
+                rospy.Subscriber('/place/goal', PlaceActionGoal, place_callback)
+                topic_active = True
+
+            if counts['0'] > 0.5:
+                try:
+                    rospy.wait_for_service('/pick_gui', timeout=5)
+                    pick_gui_service = rospy.ServiceProxy('/pick_gui', Empty)
+                    print("Grasp poses detected, starting script...")
+                    print("Made decision to pick the object")
+                    pick_gui_service()
+                except rospy.ServiceException as e:
+                    print("Service call failed: ", e)
+        except:
+            print("waiting for decision to pick the object")
+            sensorial_unit = SensorialUnit("SensorialUnit0", Ts)
+            print("switch_signal =", sensorial_unit.scalar_reading)
+            # Measure the qubits
+            shots = 1
+            counts = model.measure(shots)
+            model.encode(sensorial_unit.scalar_reading , dim=0)
+            print(model.circ)
+            print("Timeout waiting for sensory inputs, exiting...")
+            # Reinitialize the model to encode a new temporal window
+            time.sleep(5)
+            model.clear()
+
+if __name__ == '__main__':
+    tiago_brain()
