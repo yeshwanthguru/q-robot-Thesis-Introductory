cc94b14ea034dcd51016793481e1041f5169c955
yeshwanth guru krishnakumar <yeshwanth445@gmail.com>
2023-03-01 22:18:21 +0100

    test update

--- tiago_Quantum_brain.py
+++ tiago_Quantum_brain.py
@@ -1,90 +1,115 @@
 #!/usr/bin/env python3.8
 import rospy
 import time
-import random
-import json
-import logging
+import qrobot
 import numpy as np
 from std_srvs.srv import Empty
-from qrobot.qunits import SensorialUnit, QUnit
+from qrobot.qunits import SensorialUnit,QUnit
 from qrobot.models import AngularModel
-from qrobot.bursts import  OneBurst
+from qrobot.bursts import  OneBurst,ZeroBurst
 from geometry_msgs.msg import PoseArray
 from moveit_msgs.msg import PickupActionGoal, PlaceActionGoal
-
-logging.basicConfig(level=logging.WARNING)
 
 # Initialize ROS node
 rospy.init_node('Tiago_brain', anonymous=True)
 n = 1  # number of qubits
 tau = 1  # number of events for each temporal window
 # model, burst and sampling time
-model = AngularModel(n, tau)
+model = qrobot.models.AngularModel(n, tau)
 burst = OneBurst()
 Ts = 0.5  # sample every 500ms
 
-sensorial_unit1 = SensorialUnit("SensorialUnit1", Ts, default_input= (np.random.randint(500, 1000) / 1000))
-sensorial_unit2= SensorialUnit("SensorialUnit2", Ts, default_input= (np.random.randint(500, 1000) / 1000))
-sensorial_unit3 = SensorialUnit("SensorialUnit3", Ts, default_input= (np.random.randint(500, 1000) / 1000))
 
-def callback_function(msg):
-    logging.warning(msg.header)
-    for i in range(len(msg.poses)):
-        logging.warning(msg.poses[i])
+# Layer 0 - Unit 0
+l0_unit0 = qrobot.qunits.SensorialUnit("l0_unit_0", Ts=0.1)
+# Layer 0 - Unit 1
+l0_unit1 = qrobot.qunits.SensorialUnit("l0_unit_1", Ts=0.1)
+# Layer 0 - Unit 2
+l0_unit2 = qrobot.qunits.SensorialUnit("l0_unit_2", Ts=0.1)
+
+
+l0_unit0.scalar_reading = 0.5
+l0_unit1.scalar_reading = 0.3
+l0_unit2.scalar_reading = 0.4
+
+
+#Grasp pose feedback from the ros after Aurco marker detector
+def grasp_callback(msg):
+    l0_unit0.scalar_reading = (np.random.randint(500, 1000) / 1000)
+   
+
+#Pick pose feedback after the grasp pose detected through the robot camera
 def pickup_callback(msg):
-    logging.warning(msg)
-def place_callback(msg):
-    logging.warning(msg)
+    l0_unit1.scalar_reading = (np.random.randint(500, 1000) / 1000)
+    
 
-def tiago_brain():
-    last_n_qubits = [0] * n  # initialize last_n_qubits with n elements set to 0
+#Place pose feedback after the above two operation completed.
+def place_callback(msg):
+    l0_unit2.scalar_reading = (np.random.randint(500, 1000) / 1000)
+
+print(l0_unit0.scalar_reading)
+print(l0_unit1.scalar_reading)
+print(l0_unit2.scalar_reading)
+
+#Main function to trigger the robot classical picking system with the Multi qubit quantum state decision making
+def tiago_classical_pickingsystem():
+    topic_active = False
+    # Layer 1 - Unit 0
+    l1_unit0 = qrobot.qunits.QUnit(name="l1_unit0",model=AngularModel(n=1, tau=10),burst=OneBurst(),Ts=0.3,in_qunits={0: l0_unit0.id})
+    # Layer 1 - Unit 1
+    l1_unit1 = qrobot.qunits.QUnit(name="l1_unit1",model=AngularModel(n=1, tau=25),burst=ZeroBurst(),Ts=0.2,in_qunits={0: l0_unit0.id})    
+    # Layer 1 - Unit 1
+    l1_unit2 = qrobot.qunits.QUnit(name="l1_unit2",model=AngularModel(n=1, tau=25),burst=ZeroBurst(),Ts=0.2,in_qunits={0: l0_unit0.id})
+    l1_unit0.query = 0.8
+    l1_unit1.query = 0.8
+    l1_unit2.query = 0.8
+    l0_unit0.start()
+    l0_unit1.start()
+    l0_unit2.start()
+    l1_unit0.start()
+    l1_unit1.start()
+    l1_unit2.start()
     while not rospy.is_shutdown():
         try:
-            rospy.Subscriber("/grasp_poses", PoseArray, callback_function)    
-            rospy.Subscriber('/pickup/goal', PickupActionGoal, pickup_callback)   
-            rospy.Subscriber('/place/goal', PlaceActionGoal, place_callback)      
-            shots = 1
-            counts = model.measure(shots)
-            result = counts
-            last_n_qubits.append(result)
-            last_n_qubits.pop(0) # remove oldest element from the list
-            model.encode(sensorial_unit1.scalar_reading , dim=0)
-            model.encode(sensorial_unit2.scalar_reading, dim=0)
-            model.encode(sensorial_unit3.scalar_reading , dim=0)
-            print(model.circ)
-            print("Aggregated binary outcomes of the circuit:")
-            print(json.dumps(counts, sort_keys=True, indent=4))           
-            if all(last_n_qubits) == 1:
-            # Call the /pick_gui service
+            if not topic_active:
+                # Subscribe to the rostopics for the sensory inputs
+                rospy.Subscriber("/grasp_poses", PoseArray, grasp_callback)
+                rospy.Subscriber('/pickup/goal', PickupActionGoal, pickup_callback)
+                rospy.Subscriber('/place/goal', PlaceActionGoal, place_callback)
+                topic_active = True
+                # Measure the combined state of all three sensorial units
+                shots = 1
+                counts = model.measure(shots)
+                result = counts
+            #Quantum state decision to trigger the robot 
+            if l1_unit0.query >= Ts:
+                
                 try:
-                    rospy.wait_for_service('/pick_gui', timeout=1)
+                    #Triggers the picking classical system
+                    rospy.wait_for_service('/pick_gui', timeout=5)
                     pick_gui_service = rospy.ServiceProxy('/pick_gui', Empty)
-                    print("Grasp poses detected, starting script...")
-                    print("Made decision to pick the object")
+                    rospy.loginfo("Grasp poses detected, starting script...")
+                    rospy.loginfo("Made decision to pick the object")
                     pick_gui_service()
                 except rospy.ServiceException as e:
-                    print("Service call failed: ", e)
-            elif all(last_n_qubits) == 0 :
-                # Print a message waiting for decision
-                print("waiting for decision to pick the object")
-                pass
-        except rospy.ROSException as e:
-            sensorial_unit = SensorialUnit("SensorialUnit0", Ts, default_input=0)
-            sensorial_unit.scalar_reading = 0
-            print("switch_signal =", sensorial_unit.scalar_reading)
-            # Measure the qubits
-            shots = 1
-            counts = model.measure(shots)
-            result = list(counts.values())[0]
-            last_n_qubits.append(result)
-            last_n_qubits.pop(0) # remove oldest element from the list
-            print("last_n_qubits =", last_n_qubits)
-            model.encode(sensorial_unit.scalar_reading , dim=0)
-            print(model.circ)
-            print("Timeout waiting for sensory inputs, exiting...")
+                    rospy.loginfo("Service call failed: ", e)
+            elif l1_unit0.query < Ts:
+                    #Decision not made so it runs with the default 
+                    rospy.loginfo("Did not make decision ,started thinking")
+                    #when no sensory input detected Goes with the default state input
+                    rospy.loginfo("waiting for decision to pick the object")
+                    l0_unit0.stop()
+                    l0_unit1.stop()
+                    l0_unit2.stop()
+                    l1_unit0.stop()
+                    l1_unit1.stop()
+                    l1_unit2.stop()               
+        except:
+            #no sensory connection it results with the warning
+            rospy.logwarn("Timeout waiting for sensory inputs, exiting...")
             # Reinitialize the model to encode a new temporal window
             time.sleep(5)
             model.clear()
-        
+
 if __name__ == '__main__':
-    tiago_brain()
+    tiago_classical_pickingsystem()
--- ui.py
+++ ui.py
@@ -1,38 +1,45 @@
-import tkinter as tk
+#!/usr/bin/env python3.8
 import rospy
+from std_srvs.srv import Empty
+class PickGuiTrigger:
+    def __init__(self):
+        rospy.init_node('pick_gui_trigger')
+        self.pick_gui_service = rospy.ServiceProxy('/pick_gui', Empty)
+        rospy.loginfo("Initialized pick_gui_trigger")
+        self.paused = False
+        self.run()
 
-def on_select(event):
-    topic_name = listbox.get(listbox.curselection())
-    try:
-        #initialize node 
-        rospy.init_node('Topic_detector', anonymous=True)
-        # check if topic is published or not 
-        if topic_name in rospy.get_published_topics():
-            label.config(text="Topic detected", bg="green")
-            print("Topic detected:", topic_name)
-        else:
-            label.config(text="Topic not detected", bg="red")
-            print("Topic not detected:", topic_name)
-    except rospy.ROSInterruptException:
-        pass
+    def run(self):
+        try:
+            rospy.wait_for_service('/pick_gui', timeout=5)
+            rospy.loginfo("Grasp poses detected, starting script...")
+            rospy.loginfo("Made decision to pick the object")
+            self.pick_gui_service()
+        except rospy.ServiceException as e:
+            rospy.loginfo("Service call failed: ", e)
+        
+        while not rospy.is_shutdown():
+            key = input("Press 1 to trigger the picking system, 0 to pause, 2 to resume: ")
+            if key == '1':
+                try:
+                    self.paused = False
+                    self.pick_gui_service()
+                    rospy.loginfo("Grasp poses detected, starting script...")
+                    rospy.loginfo("Made decision to pick the object")
+                except rospy.ServiceException as e:
+                    rospy.loginfo("Service call failed: ", e)
+            elif key == '0':
+                self.paused = True
+                rospy.loginfo("Script paused")
+            elif key == '2':
+                self.paused = False
+                rospy.loginfo("Script resumed")
+            else:
+                rospy.loginfo("Invalid input")
 
-root = tk.Tk()
-root.title("ROS Topic Selector")
+            while self.paused and not rospy.is_shutdown():
+                rospy.sleep(0.1)
 
-listbox = tk.Listbox(root)
-listbox.pack()
+if __name__ == '__main__':
+    PickGuiTrigger()
 
-# Populate the listbox with available topics
-topics = rospy.get_published_topics()
-for topic in topics:
-    listbox.insert(tk.END, topic[0])
-
-# Populate the listbox with available topics
-topics = rospy.get_published_topics()
-for topic in topics:
-    listbox.insert(tk.END, topic[0])
-
-label = tk.Label(root, text="Select a topic from the list")
-label.pack()
-
-root.mainloop()
