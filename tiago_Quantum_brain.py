88a88c69792028929835b0d3f4c18d0f9067cce5
yeshwanth guru krishnakumar <yeshwanth445@gmail.com>
2023-03-10 19:25:00 +0100

    qbrain working updated

--- tiago_Quantum_brain.py
+++ tiago_Quantum_brain.py
@@ -1,115 +1,79 @@
-#!/usr/bin/env python3.8
-import rospy
-import time
 import qrobot
-import numpy as np
-from std_srvs.srv import Empty
-from qrobot.qunits import SensorialUnit,QUnit
+import time
 from qrobot.models import AngularModel
-from qrobot.bursts import  OneBurst,ZeroBurst
-from geometry_msgs.msg import PoseArray
-from moveit_msgs.msg import PickupActionGoal, PlaceActionGoal
+from qrobot.bursts import ZeroBurst,OneBurst
+import json
+from random import randint
+from IPython.display import clear_output
 
-# Initialize ROS node
-rospy.init_node('Tiago_brain', anonymous=True)
-n = 1  # number of qubits
-tau = 1  # number of events for each temporal window
-# model, burst and sampling time
-model = qrobot.models.AngularModel(n, tau)
-burst = OneBurst()
-Ts = 0.5  # sample every 500ms
+Ts = 0.5
+Sensorial_unit0 = qrobot.qunits.SensorialUnit("Sensorial_unit0", Ts=0.1)
+Sensorial_unit1 = qrobot.qunits.SensorialUnit("Sensorial_unit1", Ts=0.1)
+Sensorial_unit2 = qrobot.qunits.SensorialUnit("Sensorial_unit2", Ts=0.1)
 
-
-# Layer 0 - Unit 0
-l0_unit0 = qrobot.qunits.SensorialUnit("l0_unit_0", Ts=0.1)
-# Layer 0 - Unit 1
-l0_unit1 = qrobot.qunits.SensorialUnit("l0_unit_1", Ts=0.1)
-# Layer 0 - Unit 2
-l0_unit2 = qrobot.qunits.SensorialUnit("l0_unit_2", Ts=0.1)
-
-
-l0_unit0.scalar_reading = 0.5
-l0_unit1.scalar_reading = 0.3
-l0_unit2.scalar_reading = 0.4
+# Create a new QUnit that receives inputs from all three sensorial units
+Qunit = qrobot.qunits.QUnit(
+    name="Qunit",
+    model=AngularModel(n=3, tau=25),
+    burst=ZeroBurst(),
+    Ts=0.2,
+    in_qunits={
+        0: Sensorial_unit0.id,
+        1: Sensorial_unit1.id,
+        2: Sensorial_unit2.id,
+    },
+)
 
 
-#Grasp pose feedback from the ros after Aurco marker detector
-def grasp_callback(msg):
-    l0_unit0.scalar_reading = (np.random.randint(500, 1000) / 1000)
-   
+Qunit.query = [0.8,0.5,0.6]
 
-#Pick pose feedback after the grasp pose detected through the robot camera
-def pickup_callback(msg):
-    l0_unit1.scalar_reading = (np.random.randint(500, 1000) / 1000)
-    
 
-#Place pose feedback after the above two operation completed.
-def place_callback(msg):
-    l0_unit2.scalar_reading = (np.random.randint(500, 1000) / 1000)
+Sensorial_unit0.start()
+Sensorial_unit1.start()
+Sensorial_unit2.start()
+Qunit.start()
 
-print(l0_unit0.scalar_reading)
-print(l0_unit1.scalar_reading)
-print(l0_unit2.scalar_reading)
+statuses = []
+refresh_time = 0.5  # Read statuses every 0.5 seconds
 
-#Main function to trigger the robot classical picking system with the Multi qubit quantum state decision making
-def tiago_classical_pickingsystem():
-    topic_active = False
-    # Layer 1 - Unit 0
-    l1_unit0 = qrobot.qunits.QUnit(name="l1_unit0",model=AngularModel(n=1, tau=10),burst=OneBurst(),Ts=0.3,in_qunits={0: l0_unit0.id})
-    # Layer 1 - Unit 1
-    l1_unit1 = qrobot.qunits.QUnit(name="l1_unit1",model=AngularModel(n=1, tau=25),burst=ZeroBurst(),Ts=0.2,in_qunits={0: l0_unit0.id})    
-    # Layer 1 - Unit 1
-    l1_unit2 = qrobot.qunits.QUnit(name="l1_unit2",model=AngularModel(n=1, tau=25),burst=ZeroBurst(),Ts=0.2,in_qunits={0: l0_unit0.id})
-    l1_unit0.query = 0.8
-    l1_unit1.query = 0.8
-    l1_unit2.query = 0.8
-    l0_unit0.start()
-    l0_unit1.start()
-    l0_unit2.start()
-    l1_unit0.start()
-    l1_unit1.start()
-    l1_unit2.start()
-    while not rospy.is_shutdown():
-        try:
-            if not topic_active:
-                # Subscribe to the rostopics for the sensory inputs
-                rospy.Subscriber("/grasp_poses", PoseArray, grasp_callback)
-                rospy.Subscriber('/pickup/goal', PickupActionGoal, pickup_callback)
-                rospy.Subscriber('/place/goal', PlaceActionGoal, place_callback)
-                topic_active = True
-                # Measure the combined state of all three sensorial units
-                shots = 1
-                counts = model.measure(shots)
-                result = counts
-            #Quantum state decision to trigger the robot 
-            if l1_unit0.query >= Ts:
+for i in range(int(10 * (1 / refresh_time))):
+    # Wait and then clean the output
+    time.sleep(refresh_time)
+    clear_output(wait=True)
+
+    # Change input every 2 second
+    if (i * refresh_time) % 2 == 0:
+        Sensorial_unit0.scalar_reading = randint(0, 500) / 1000
+        Sensorial_unit1.scalar_reading = randint(0, 500) / 1000
+        Sensorial_unit2.scalar_reading = randint(0, 500) / 1000
+
+    # Read statused and store it
+    status = qrobot.qunits.redis_utils.redis_status()
+    statuses.append(status)
+
+    # Extract output values for all QUnits
+    for key, value in status.items():
+        if ' class' in key and value == 'QUnit':
+            qunit_name = key[:-6]  # Remove ' output' suffix
+            qunit_output = status.get(f'{qunit_name} output')
+            if qunit_output is not None:
+                qunit_output = float(qunit_output)  # Convert to float
+                if qunit_output > 0.5:
+                    print("hi")
+                else:
+                    print("bye")
+                print(f'{qunit_name} output:', qunit_output)
                 
-                try:
-                    #Triggers the picking classical system
-                    rospy.wait_for_service('/pick_gui', timeout=5)
-                    pick_gui_service = rospy.ServiceProxy('/pick_gui', Empty)
-                    rospy.loginfo("Grasp poses detected, starting script...")
-                    rospy.loginfo("Made decision to pick the object")
-                    pick_gui_service()
-                except rospy.ServiceException as e:
-                    rospy.loginfo("Service call failed: ", e)
-            elif l1_unit0.query < Ts:
-                    #Decision not made so it runs with the default 
-                    rospy.loginfo("Did not make decision ,started thinking")
-                    #when no sensory input detected Goes with the default state input
-                    rospy.loginfo("waiting for decision to pick the object")
-                    l0_unit0.stop()
-                    l0_unit1.stop()
-                    l0_unit2.stop()
-                    l1_unit0.stop()
-                    l1_unit1.stop()
-                    l1_unit2.stop()               
-        except:
-            #no sensory connection it results with the warning
-            rospy.logwarn("Timeout waiting for sensory inputs, exiting...")
-            # Reinitialize the model to encode a new temporal window
-            time.sleep(5)
-            model.clear()
+    # Print output
+    print(json.dumps(status, indent=1, sort_keys=True))
+    print(int(i * refresh_time), "/30 seconds")
 
-if __name__ == '__main__':
-    tiago_classical_pickingsystem()
+
+    # Plot graph
+    qbrain_graph = qrobot.graph(status)
+    qrobot.draw(qbrain_graph)
+
+Sensorial_unit0.stop()
+Sensorial_unit1.stop()
+Sensorial_unit2.stop()
+Qunit.stop()
