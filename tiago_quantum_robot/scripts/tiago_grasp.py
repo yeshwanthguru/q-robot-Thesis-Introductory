bc6df3ec7241d78ad61ce19e41950727767d1e5e
yeshwanth guru krishnakumar <72270080+yeshwanthguru@users.noreply.github.com>
2023-03-16 15:07:26 +0100

    Update tiago_grasp.py
    update

--- tiago_quantum_robot/scripts/tiago_grasp.py
+++ tiago_quantum_robot/scripts/tiago_grasp.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 import rospy
+import os
 from geometry_msgs.msg import Pose, PoseStamped
+
+LOG_FILE_PATH = '/home/yg/tiago_public_ws/src/tiago_quantum_robot/data/grasp_pose.log'
 
 def callback(data):
     # Invert the received pose
@@ -17,13 +20,22 @@ def callback(data):
     # Publish the inverted pose on the /grasp_pose topic
     grasp_pose_pub.publish(PoseStamped(header=data.header, pose=inverted_pose))
     
-    # Log the inverted pose to a file
-    with open('inverted_poses.log', 'a') as f:
+    # Log the inverted pose in the terminal
+    rospy.loginfo("Inverted pose:\n%s", inverted_pose)
+    
+    # Log the inverted pose in the file
+    with open(LOG_FILE_PATH, 'a') as f:
         f.write(str(inverted_pose) + '\n')
 
 if __name__ == '__main__':
+    # Create the directory for the log file if it doesn't exist
+    log_dir = os.path.dirname(LOG_FILE_PATH)
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
     rospy.init_node('pose_inverter', anonymous=True)
     detected_pose_sub = rospy.Subscriber('/detected_aruco_pose', PoseStamped, callback)
     grasp_pose_pub = rospy.Publisher('/grasp_pose', PoseStamped, queue_size=1)
     rospy.spin()
+
 
