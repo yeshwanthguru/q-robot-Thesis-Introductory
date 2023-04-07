34c5884fb7b0329609b354d4a274ca075b277d4d
yeshwanth guru krishnakumar <72270080+yeshwanthguru@users.noreply.github.com>
2023-04-07 10:42:02 +0200

    Update tiago_grasp.py
    moveit plan to grasp the object _test update

--- tiago_quantum_robot/scripts/tiago_grasp.py
+++ tiago_quantum_robot/scripts/tiago_grasp.py
@@ -1,35 +1,125 @@
 #!/usr/bin/env python
-
 import rospy
-import os
 from geometry_msgs.msg import PoseStamped
-import tf.transformations as tr
+from tf.transformations import *
+from tf2_geometry_msgs import *
+from geometry_msgs.msg import Quaternion
+import moveit_commander
+import sys
+from std_srvs.srv import Empty, EmptyResponse
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+import time
 
-def transform_pose(pose_in):
-    # Get the translation and rotation from the input pose
-    pos_in = pose_in.pose.position
-    ori_in = pose_in.pose.orientation
-    mat_rot_x = tr.rotation_matrix(3.142, [1,0,0])
-    rot_in = tr.quaternion_matrix([ori_in.x, ori_in.y, ori_in.z, ori_in.w])
-    rot_out = mat_rot_x.dot(rot_in)
-    ori_out = tr.quaternion_from_matrix(rot_out)
-    # Construct the transformed pose
-    pose_out = PoseStamped()
-    pose_out.header = pose_in.header
-    pose_out.pose.position = pos_in
-    pose_out.pose.orientation.x = ori_out[0]
-    pose_out.pose.orientation.y = ori_out[1]
-    pose_out.pose.orientation.z = ori_out[2]
-    pose_out.pose.orientation.w = ori_out[3]
-    return pose_out
+global robot
+global move_group
+global grasp_pose
+global object_pose
 
-def callback(data):
-    # Transform the received pose and publish the result
-    transformed_pose = transform_pose(data)
-    pub.publish(transformed_pose)   
+# flag to indicate if pose has been received
+pose_received = False
+grasped = False
+
+# define the desired values here
+z_axis_offset = 0.1
+y_axis_offset = 0.3
+
+def reach_goal(data):
+    global reach_goal
+    positions = data.points[0].positions
+    rospy.loginfo('Received Joint Positions: {}'.format(positions))
+    reach_goal = positions
+
+def close_gripper():
+    pub_gripper_controller = rospy.Publisher('/gripper_controller/command', JointTrajectory, queue_size=1)
+    for i in range(10):
+        trajectory = JointTrajectory()
+        trajectory.joint_names = [
+            'gripper_left_finger_joint', 'gripper_right_finger_joint']
+        trajectory_points = JointTrajectoryPoint()
+        trajectory_points.positions = [0.0, 0.0]
+        trajectory_points.time_from_start = rospy.Duration(1.0)
+        trajectory.points.append(trajectory_points)
+        pub_gripper_controller.publish(trajectory)
+        rospy.loginfo('close_gripper')
+        rospy.sleep(0.1)
+
+    
+def open_gripper():
+    pub_gripper_controller = rospy.Publisher('/gripper_controller/command', JointTrajectory, queue_size=1)
+    for i in range(10):
+        trajectory = JointTrajectory()
+        trajectory.joint_names = [
+            'gripper_left_finger_joint', 'gripper_right_finger_joint']
+        trajectory_points = JointTrajectoryPoint()
+        trajectory_points.positions = [0.044, 0.044]
+        trajectory_points.time_from_start = rospy.Duration(1.0)
+        trajectory.points.append(trajectory_points)
+        pub_gripper_controller.publish(trajectory)
+        rospy.loginfo('open_gripper')
+        rospy.sleep(0.1)
+
+    
+def grasp_pose_callback(msg):
+    global grasp_pose    
+    global object_pose
+    global move_group
+    global pose_received    
+    global grasped
+    if not pose_received:
+        pose_received = True
+        object_pose = msg
+        object_pose.pose.orientation = Quaternion(0.5, 0.5, 0.5, 0.5)
+        object_pose.pose.position.z += z_axis_offset     
+        grasp_pose = msg.pose            
+        grasp_pose.position.y -= y_axis_offset  
+        object_pose.pose.position.y = -0.2
+        rospy.loginfo('PickObject - attempting to reach pre-grasp pose')       
+        move_group.set_pose_target(object_pose.pose)
+        plan = move_group.go(wait=True)
+        move_group.stop()
+        move_group.clear_pose_targets()
+        rospy.loginfo('PickObject:reached')
+        
+        joint_goal = [0.31, 0.28, -0.42, -1.63, 1.71, -2.07, 1.25, 1.62]
+        move_group.go(joint_goal, wait=True)
+        move_group.stop()     
+        rospy.sleep(1)
+        rospy.loginfo('Grasping Object...')
+        close_gripper()
+        grasped = True
+        
+    elif grasped:
+        # Lift object
+        object_pose.pose.position.z += 0.1
+        move_group.set_pose_target(object_pose.pose)
+        plan = move_group.go(wait=True)
+        move_group.stop()
+        move_group.clear_pose_targets()
+        rospy.loginfo('lifting the object')
+        grasped = False
+        
+        joint_goal = reach_goal
+        move_group.go(joint_goal, wait=True)
+        move_group.stop()
+        rospy.sleep(1)        
+        rospy.loginfo('Releasing Object...')
+        open_gripper()
+        rospy.sleep(1)
+        rospy.loginfo('Returning Object to Initial Position...')
+        move_group.set_pose_target(object_pose.pose)
+        plan = move_group.go(wait=True)
+        move_group.stop()
+        move_group.clear_pose_targets()
+        
+
 if __name__ == '__main__':
-    rospy.init_node('pose_transformer', anonymous=True)
-    # Setup subscriber to detected_aruco_pose and publisher to transformed_pose
-    detected_pose_sub = rospy.Subscriber('/detected_aruco_pose', PoseStamped, callback)
-    pub = rospy.Publisher('/grasp_pose', PoseStamped, queue_size=10)
+    rospy.init_node('grasp_pose_subscriber')
+    # initialize moveit commander
+    moveit_commander.roscpp_initialize(sys.argv)
+    robot = moveit_commander.RobotCommander()
+    # define group of joint to adjust the height of TIAGo
+    move_group = moveit_commander.MoveGroupCommander('arm_torso')      
+    rospy.Subscriber('/grasp_pose', PoseStamped, grasp_pose_callback)
+    rospy.Subscriber('/reach_goal', JointTrajectory, reach_goal)
+    rospy.loginfo("PickObject - Services ready")
     rospy.spin()
